55ccbf5fec58e6b29fb607c9bed6f4483428e8ec
danielgregui <danielgregui@gmail.com>
2022-07-31 23:44:03 -0300

    Criando Controlador Scaffold

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
 
 namespace WebApplication2
 {
@@ -33,6 +35,9 @@ namespace WebApplication2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplication2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
